e98ffa5d5e58780424c1b157c532d8413563d138
Br12plo <146698743+Br12plo@users.noreply.github.com>
2025-09-06 17:36:06 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,33 @@
-// custom cursor
-const cursor = document.createElement("div");
-cursor.classList.add("cursor");
-document.body.appendChild(cursor);
+document.addEventListener("DOMContentLoaded", () => {
+  // custom cursor
+  const cursor = document.createElement("div");
+  cursor.classList.add("cursor");
+  document.body.appendChild(cursor);
 
-document.addEventListener("mousemove", e => {
+  document.addEventListener("mousemove", e => {
     cursor.style.top = e.clientY + "px";
     cursor.style.left = e.clientX + "px";
+  });
+
+  // dark mode toggle
+  const toggle = document.querySelector(".dark-toggle");
+  if (toggle) {
+    toggle.addEventListener("click", () => {
+      document.body.classList.toggle("dark");
+    });
+  }
+
+  // typewriter effect JS
+  const typewriter = document.getElementById("typewriter");
+  if (typewriter) {
+    const text = typewriter.textContent;
+    typewriter.textContent = "";
+    let i = 0;
+    const typing = setInterval(() => {
+      typewriter.textContent += text[i];
+      i++;
+      if (i === text.length) clearInterval(typing);
+    }, 100);
+  }
 });
 
-// dark mode toggle
-const toggle = document.querySelector(".dark-toggle");
-toggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-});
